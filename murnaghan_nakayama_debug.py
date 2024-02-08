a671865a8f409259744e380ea228b6704275b38f
athu1248 <atharvak@umich.edu>
2024-02-08 14:13:20 -0500

    Changed To Use Only Strings (Reduces Time in Conversion)

--- murnaghan_nakayama_debug.py
+++ murnaghan_nakayama_debug.py
@@ -4,46 +4,39 @@ import numpy as np
 from sympy.utilities.iterables import partitions
 
 
-def list_to_string(arr: list) -> str:
-    return "".join([str(elem) for elem in arr])
-
-
-def string_to_list(s: str) -> list:
-    return [int(char) for char in s]
-
-
-def make_bit_strings(parts: list) -> list:
+def make_bit_strings(parts: list) -> list[str]:
     bit_strings = []
     for lambda_ in parts:
         # Iterate through each partition
         # Make our bit string
-        arr = []
+        curr_bit_str = ""
         prev = 0
         lamda_prime = reversed(list(lambda_.keys()))
         for key in list(lamda_prime):
             for _ in range(key - prev):
-                arr.append(0)
+                curr_bit_str += '0'
             for _ in range(lambda_[key]):
-                arr.append(1)
+                curr_bit_str += '1'
             prev = key
 
-        bit_strings.append(list_to_string(arr))
+        bit_strings.append(curr_bit_str)
     return bit_strings
 
 
-def validate_bit_string(bit_string: list):
+def validate_bit_string(bit_string: str) -> str:
     # Remove 1's from start of bit_string
     while len(bit_string) > 0:
-        if bit_string[0] != 0:
-            del bit_string[0]
+        if bit_string[0] == '1':
+            bit_string = bit_string[1:]
         else:
             break
     # Remove 0's from end of bit_string
     while len(bit_string) > 0:
-        if bit_string[-1] != 1:
-            del bit_string[-1]
+        if bit_string[-1] == '0':
+            bit_string = bit_string[:-1]
         else:
             break
+    return bit_string
 
 
 def get_cycle_lengths(perm: list) -> dict:
@@ -52,19 +45,32 @@ def get_cycle_lengths(perm: list) -> dict:
     curr = 0
     j = 0
     for i, val in enumerate(perm):
-        if val == 0:
+        if val == '0':
             curr += 1
-        if val == 1:
+        if val == '1':
             if curr not in cycle_lengths:
                 cycle_lengths[curr] = (j, i)
             j = i + 1
     return cycle_lengths
 
 
-def murnaghan_nakayama(n: int, lambda_p, sigma_p):
+def get_erased_bs(lambda_: str, i: int, j: int) -> str:
+    erased_bs = ''
+    for idx, c in enumerate(lambda_):
+        if idx == i:
+            erased_bs += lambda_[j]
+        elif idx == j:
+            erased_bs += lambda_[i]
+        else:
+            erased_bs += c
+    erased_bs = validate_bit_string(erased_bs)
+    return erased_bs
+
+
+def murnaghan_nakayama(n: int, lambda_: str, sigma: str):
     # TODO: I think n is not a required argument, we only care about the partitions
-    lambda_ = string_to_list(lambda_p)
-    sigma = string_to_list(sigma_p)
+    # lambda_ = string_to_list(lambda_p)
+    # sigma = string_to_list(sigma_p)
 
     # Base Cases
     if len(lambda_) == 0 and len(sigma) == 0: # X_0(0) = 1
@@ -75,21 +81,21 @@ def murnaghan_nakayama(n: int, lambda_p, sigma_p):
     #     return 0
 
     # DP
-    if (n, lambda_p, sigma_p) in memo:
-        return memo[(n, lambda_p, sigma_p)]
+    if (n, lambda_, sigma) in memo:
+        return memo[(n, lambda_, sigma)]
 
     # Get our hooks
     hooks = [
         (x, y)
         for x in range(len(lambda_))
         for y in range(len(lambda_))
-        if (lambda_[x] == 0 and lambda_[y] == 1)
+        if (lambda_[x] == '0' and lambda_[y] == '1')
     ]
 
     # Invalid diagram -- return 0
     if len(hooks) == 0:
-        memo[(n, lambda_p, sigma_p)] = 0
-        return memo[(n, lambda_p, sigma_p)]
+        memo[(n, lambda_, sigma)] = 0
+        return memo[(n, lambda_, sigma)]
 
     cycle_lengths = get_cycle_lengths(sigma)
 
@@ -103,23 +109,20 @@ def murnaghan_nakayama(n: int, lambda_p, sigma_p):
             # Now need to find rho and tau
             # Just now need to remove the actual row
             tau = sigma[: cycle_lengths[t][0] + 1] + sigma[cycle_lengths[t][1] + 1 :]
-            validate_bit_string(tau)
+            tau = validate_bit_string(tau)
 
-            height = lambda_[i:j].count(1)
+            height = lambda_[i:j].count('1')
 
-            erased_bs = lambda_.copy()
-            erased_bs[i] = lambda_[j]
-            erased_bs[j] = lambda_[i]
-            validate_bit_string(erased_bs)
+            erased_bs = get_erased_bs(lambda_, i, j)
 
             temp = ((-1) ** height) * murnaghan_nakayama(
-                n - t, list_to_string(erased_bs), list_to_string(tau)
+                n - t, erased_bs, tau
             )
             mn_sum += temp
 
     # Done!
-    memo[(n, lambda_p, sigma_p)] = mn_sum
-    return memo[(n, lambda_p, sigma_p)]
+    memo[(n, lambda_, sigma)] = mn_sum
+    return memo[(n, lambda_, sigma)]
 
 
 def get_character_table(N: int):
