4f4db40e25939033d155c5e006b76ca33eb3fb0a
Guanyu Zhang <108467515+TonyZhang2004@users.noreply.github.com>
2024-03-25 13:26:21 -0400

    added some remarks about frobenius formula

--- frobenius.py
+++ frobenius.py
@@ -8,7 +8,7 @@ R, *x = ring("x_0 x_1 x_2 x_3 x_4 x_5 x_6 x_7 x_8 x_9 x_10", ZZ)
 
 def convert_dict_partitions_to_list(n: int):
     """
-    TODO: What does this funciton do?
+    uses the builtin partition module and convert that to a python list
     """
     index = list(partitions(n))
     partitions_list = []
@@ -22,15 +22,24 @@ def convert_dict_partitions_to_list(n: int):
 
 
 def Delta(k: int):
+    """
+    The anti-symmetrizer in the Frobenius formula
+    """
     return R.mul([x[i] - x[j] for j in range(2, k + 1) for i in range(1, j)])
 
 
 def P(j: int, k: int):
+    """
+    The product of power sum in the Frobenius formula
+    """
     # return the k-th power sum of the list of variables x
     return sum([x[i] ** j for i in range(1, k + 1)])
 
 
 def frobenius(irrep: List[int], conj_class: List[int]):
+    """
+    calculate the product of the polynomial and get the character value using the monomial that corresponds to the Specht Module
+    """
     kv = {}
     for num in conj_class:
         if num not in kv:
@@ -46,6 +55,9 @@ def frobenius(irrep: List[int], conj_class: List[int]):
 
 
 def get_character_table(n: int):
+    """
+    get the final character table by computing each slot using the Frobenius formula.
+    """
     partition_list = convert_dict_partitions_to_list(n)
     character_table = [
         [0 for _ in range(len(partition_list))] for _ in range(len(partition_list))
