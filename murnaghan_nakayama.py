68e97c55ca9d8640f86f57445f0ca07d34c1c8ec
athu1248 <atharvak@umich.edu>
2024-03-25 10:54:49 -0400

    Documentation for Murnaghan_Nakayama File

--- murnaghan_nakayama.py
+++ murnaghan_nakayama.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Tuple
+from typing import Dict, List
 import pickle
 import numpy as np
 from sympy.utilities.iterables import partitions
@@ -8,6 +8,13 @@ MEMO = {}
 
 
 def make_bit_strings(parts: List[Dict[int, int]]) -> List[str]:
+    """
+    Returns a list of partitions in abaci format bit strings.
+    Parameters:
+    parts: List of partitions where each partition is given by
+    a dictionary whose key is the number appearing in the
+    partition and the value is the number of times it appears.
+    """
     bit_strings = []
     for part in parts:
         # Iterate through each partition
@@ -25,6 +32,10 @@ def make_bit_strings(parts: List[Dict[int, int]]) -> List[str]:
 
 
 def validate_bit_string(bit_string: str) -> str:
+    """
+    Returns validated abaci bit string which must start with
+    a 0 and end with a 1.
+    """
     # Remove 1's from start of bit_string
     while len(bit_string) > 0:
         if bit_string[0] == "1":
@@ -40,29 +51,23 @@ def validate_bit_string(bit_string: str) -> str:
     return bit_string
 
 
-def get_cycle_lengths(perm: str) -> Dict[int, Tuple[int, int]]:
-    # key is cycle length and value is starting 0 and ending 1
-    cycle_lengths = {}
-    curr = 0
-    j = 0
-    for i, val in enumerate(perm):
-        if val == "0":
-            curr += 1
-        if val == "1":
-            if curr not in cycle_lengths:
-                cycle_lengths[curr] = (j, i)
-            j = i + 1
-    return cycle_lengths
-
-
-def get_tau(sigma: str, j: int) -> str:
+def get_tau(sigma: str) -> str:
+    """
+    Returns the abaci bit string of a partition after removing
+    the top-most row, i.e., the largest cycle type.
+    """
     # j := idx of 1
-    tau = sigma[:j] + sigma[(j + 1):]  # just remove the 1
+    tau = sigma[:-1]  # just remove the 1
     tau = validate_bit_string(tau)
     return tau
 
 
 def get_erased_bs(lambda_: str, i: int, j: int) -> str:
+    """
+    Returns the abaci bit string of a partition after removing
+    the border strip corresponding to a hook starting at index
+    i and ending at index j.
+    """
     erased_bs = ""
     for idx, c in enumerate(lambda_):
         if idx == i:
@@ -75,19 +80,24 @@ def get_erased_bs(lambda_: str, i: int, j: int) -> str:
     return erased_bs
 
 
-def murnaghan_nakayama(n: int, lambda_: str, sigma: str) -> int:
-    # TODO: I think n is not a required argument, we only care about the partitions
+def murnaghan_nakayama(lambda_: str, sigma: str) -> int:
+    """
+    Returns the character value corresponding to the partitions
+    lambda_ (row) and sigma (column) using the recursive
+    Murnaghan-Nakayama Rule.
+    """
     global MEMO
 
     # Base Cases
-    if len(lambda_) == 0 and len(sigma) == 0:  # X_0(0) = 1
+    if len(lambda_) == 0 and len(sigma) == 0:  # X^0(0) = 1
+        return 1
+    if len(lambda_) == 1 and len(sigma) == 1:  # X^1(1) = 1
         return 1
 
     # DP
-    if (n, lambda_, sigma) in MEMO:
-        return MEMO[(n, lambda_, sigma)]
+    if (lambda_, sigma) in MEMO:
+        return MEMO[(lambda_, sigma)]
 
-    # Get our hooks
     hooks = [
         (x, y)
         for x in range(len(lambda_))
@@ -97,32 +107,41 @@ def murnaghan_nakayama(n: int, lambda_: str, sigma: str) -> int:
 
     # Invalid diagram -- return 0
     if len(hooks) == 0:
-        MEMO[(n, lambda_, sigma)] = 0
-        return MEMO[(n, lambda_, sigma)]
+        MEMO[(lambda_, sigma)] = 0
+        return MEMO[(lambda_, sigma)]
 
-    cycle_lengths = get_cycle_lengths(sigma)
-
-    max_cycle_length = max(list(cycle_lengths.keys()))
-    tau = get_tau(sigma, cycle_lengths[max_cycle_length][1])
+    tau = get_tau(sigma)
+    cycle_length_removed = 0
+    for c in sigma:
+        if c == "0":
+            cycle_length_removed += 1
 
     mn_sum = 0
     # Loop through our hooks
     for hook in hooks:
         i, j = hook
         t = j - i  # formula for hook length
-        if t == max_cycle_length:
+        if t == cycle_length_removed:
             height = lambda_[i:j].count("1")
             erased_bs = get_erased_bs(lambda_, i, j)
-            temp = ((-1) ** height) * murnaghan_nakayama(n - t, erased_bs, tau)
+            temp = ((-1) ** height) * murnaghan_nakayama(erased_bs, tau)
             mn_sum += temp
 
-    MEMO[(n, lambda_, sigma)] = mn_sum
-    return MEMO[(n, lambda_, sigma)]
+    MEMO[(lambda_, sigma)] = mn_sum
+    return MEMO[(lambda_, sigma)]
 
 
 def get_character_table(
-    n: int, memo_file_name: str = "memo.txt", csv_file_name: str = ""
+    n: int, output_file_name: str = "", memo_file_name: str = "memo.txt"
 ):
+    """
+    Returns the whole character table for n, i.e., the character
+    table of Sn
+    Parameters:
+    n: The number for which character table is copmuted
+    output_file_name: file to store character table
+    memo_file_name: read/write from existing memo file
+    """
     parts = list(partitions(n))  # Get list of partitions
     bit_strings = make_bit_strings(parts)
 
@@ -133,7 +152,7 @@ def get_character_table(
     for lambda_ in bit_strings:
         curr_row = []
         for sigma in bit_strings:
-            val = murnaghan_nakayama(n, lambda_, sigma)
+            val = murnaghan_nakayama(lambda_, sigma)
             curr_row.append(val)
         char_table.append(curr_row)
 
@@ -141,18 +160,24 @@ def get_character_table(
         write_memo_to_file(memo_file_name)
     char_table = np.fliplr(np.array(char_table, dtype=np.int64))
 
-    if csv_file_name:
-        np.savetxt(csv_file_name, char_table, delimiter=",", fmt="%d")
+    if output_file_name:
+        np.savetxt(output_file_name, char_table, delimiter=",", fmt="%d")
 
     return char_table
 
 
 def get_character_value_of_column(
-    n: int,
-    memo_file_name: str = "memo.txt",
-    csv_file_name: str = "",
-    col_bit_string="",
+    n: int, col_bit_string="", csv_file_name: str = "", memo_file_name: str = "memo.txt"
 ):
+    """
+    Returns the character values for just for a column in Sn
+    Parameters:
+    n: The number for which character table is copmuted
+    col_bit_string: the abaci bit string value of the column
+    for which the characater values will be computed
+    output_file_name: file to store character table
+    memo_file_name: read/write from existing memo file
+    """
     parts = list(partitions(n))  # Get list of partitions
     bit_strings = make_bit_strings(parts)
 
@@ -163,7 +188,7 @@ def get_character_value_of_column(
     for lambda_ in bit_strings:
         curr_row = []
         sigma = col_bit_string
-        val = murnaghan_nakayama(n, lambda_, sigma)
+        val = murnaghan_nakayama(lambda_, sigma)
         curr_row.append(val)
         char_table.append(curr_row)
 
@@ -178,6 +203,9 @@ def get_character_value_of_column(
 
 
 def read_memo_from_file(file_name: str = "memo.txt"):
+    """
+    Read existing memo file.
+    """
     try:
         with open(file_name, "rb") as memo_file:
             global MEMO
@@ -187,18 +215,17 @@ def read_memo_from_file(file_name: str = "memo.txt"):
 
 
 def write_memo_to_file(file_name: str = "memo.txt"):
+    """
+    Save memo to a file.
+    """
     with open(file_name, "wb") as memo_file:
         global MEMO
         pickle.dump(MEMO, memo_file)
 
 
 if __name__ == "__main__":
-    i = 1
-    N = 1
-    while True:
-        my_char_table = get_character_value_of_column(
-            N, memo_file_name="memo2.txt", csv_file_name=f"S{N}_square.csv", col_bit_string="01" + ("001" * (i-1))
-        )
-        print(f"Done for {N}!")
-        i += 1
-        N = i**2
+    N = 78
+    i = 12
+    get_character_value_of_column(
+        N, f"S{N}_staircase.csv", "memo_staircase.txt", "01" * i
+    )
